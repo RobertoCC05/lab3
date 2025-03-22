d5703a60560c1b52bc10d2cbe9ee94ce8502860f
alexvillegasopenpath <alex@openpath.io>
2025-03-22 11:09:46 -0600

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,17 @@
-import { useState, useRef  } from 'react'
 import './App.css'
+import Login from './Components/Login'
+import Welcome from './Components/Welcome'
+import { AuthContext } from './Context/AuthContext'
+import { useContext } from 'react'
 
 
 function App() {
 
-    const emailRef = useRef()
-    const passwordRef = useRef('')
-    const [error, setError] = useState('')
-    const [user, setUser] = useState(null)
-    
-
-const login = (email, password) => {
-  // Fake login check
-  if (email === "admin" && password === "1234") {        
-    return true;
-  }
-  return false;
-};
-
-
-const handleLogin = () => {
-   const email = emailRef.current.value;
-   const password = passwordRef.current.value;
-   const isSuccess = login(email, password);
-   if(!isSuccess)   
-   {
-      setError("Credenciales incorrectas");
-   }
-   else
-   {
-     setUser({email})
-   }
-}
+  const { user } = useContext(AuthContext)
 
   return (
     <>
-
-           <h2>Login</h2>
-        
-          
-          
-              <div>
-                <input 
-                type="email"
-                placeholder='Correo'
-                ref={emailRef}
-                />
-                <br/>
-                <input
-                type="password"
-                placeholder='Contraseña'
-                ref={passwordRef}
-                />
-                <br/>
-                <button onClick={handleLogin}>Ingresar</button>
-                { error && <p style={{ color: "red" }}>{error}</p> }
-              </div>
-            
-          
-        
-
-                
+      { user ? <Welcome userName={user} /> : <Login/>}             
     </>
   )
 }
