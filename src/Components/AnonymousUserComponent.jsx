f0887d7c923f2632a229394aacfccb691123a27b
alexvillegasopenpath <alex@openpath.io>
2025-03-22 08:53:24 -0600

    FIX - project

---
+++ src/Components/AnonymousUserComponent.jsx
@@ -0,0 +1,11 @@
+
+
+
+export default function AnonymousUserComponent() {
+
+    return (
+        <>
+            <h2>Hola, por favor accede al sition para obtener mayor informacion</h2>
+        </>
+    )
+}
